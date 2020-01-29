7ed9737102dc68565588f33045b7c8a338ae6351
kuldeepbrd1 <31183061+kuldeepbrd1@users.noreply.github.com>
2020-01-29 04:13:43 +0100

    Updated for SPEED bbox dataset
    Works with xml annotations

--- voc_annotation.py
+++ voc_annotation.py
@@ -1,14 +1,20 @@
 import xml.etree.ElementTree as ET
 import tensorflow as tf
-from os import path
+import os
 import numpy as np
+import time
 
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
-tfrecords_size = 1000
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+start_time = time.time()
 
+classes = ["Tango"]
+tfrecords_size = 12001
+
+def _bytes_feature(value):
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 def convert_to_tfrecord(xml, record_writer):
-    name, _ = xml.split('/')[-1].split('.')
+    name, _ = xml.split('\\')[-1].split('.')
     root = ET.parse(xml.encode('utf-8')).getroot()
     xmins = []
     ymins = []
@@ -28,14 +34,15 @@ def convert_to_tfrecord(xml, record_writer):
         ymaxs.append(float(xmlbox.find('ymax').text))
         labels.append(int(cls_id))
 
+    image_path = os.path.join('speed','images','train')
     image_data = tf.io.read_file(
-        tf.io.gfile.glob('%s/%s/**/%s.jp*g' % (clazz, file, name))[0])
+        tf.io.gfile.glob(os.path.join(image_path,('%s.jp*g' % (name))))[0])
     example = tf.train.Example(features=tf.train.Features(
         feature={
             'image/encoded':
-            tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_data])),
+            tf.train.Feature(bytes_list=tf.train.BytesList(value=[str(image_data).encode('utf-8')])),
             'image/object/bbox/name':
-            tf.train.Feature(bytes_list=tf.train.BytesList(value=[name])),
+            tf.train.Feature(bytes_list=tf.train.BytesList(value=[name.encode('utf-8')])),
             'image/object/bbox/xmin':
             tf.train.Feature(float_list=tf.train.FloatList(value=xmins)),
             'image/object/bbox/xmax':
@@ -54,29 +61,33 @@ for clazz in classes:
     index_records = 1
     num = 1
     record_writer = tf.io.TFRecordWriter(
-        path.join('./', 'cci_%d_%s.tfrecords' % (index_records, clazz)))
+        os.path.join('./', 'cci_%d_%s.tfrecords' % (index_records, clazz)))
 
-    for file in tf.io.gfile.listdir(clazz):
-        if tf.io.gfile.isdir('%s/%s' % (clazz, file)):
-            xmls = tf.io.gfile.glob('%s/%s/**/*.xml' % (clazz, file))
-            np.random.shuffle(xmls)
-            for xml in xmls:
-                if num >= tfrecords_size:
-                    tf.io.gfile.rename(
-                        'cci_%d_%s.tfrecords' % (index_records, clazz),
-                        'cci_%d_%s_%d.tfrecords' % (index_records, clazz, num))
-                    index_records += 1
-                    num = 1
-                    record_writer.close()
-                    record_writer = tf.io.TFRecordWriter(
-                        path.join(
-                            './',
-                            'cci_%d_%s.tfrecords' % (index_records, clazz)))
-                convert_to_tfrecord(xml, record_writer)
-                num += 1
-            tf.io.gfile.rename(
-                'cci_%d_%s.tfrecords' % (index_records, clazz),
-                'cci_%d_%s_%d.tfrecords' % (index_records, clazz, num))
+    for file in tf.io.gfile.listdir(os.path.join('speed','detection_annotations')):
+        
+        xmls_path = os.path.join('speed','detection_annotations','xml')
+        xmls = tf.io.gfile.glob(os.path.join(xmls_path,'img******.xml'))
+        np.random.shuffle(xmls)
+        for idx,xml in enumerate(xmls):
+            if (idx % 100 ==0):
+                print("--- %d images recorded --- Time Elapsed: %s seconds ---"  %  (idx,time.time() - start_time))
+            if num >= tfrecords_size:
+                tf.io.gfile.rename(
+                    'cci_%d_%s.tfrecords' % (index_records, clazz),
+                    'cci_%d_%s_%d.tfrecords' % (index_records, clazz, num))
+                index_records += 1
+                num = 1
+                record_writer.close()
+                record_writer = tf.io.TFRecordWriter(
+                        os.path.join(
+                        './',
+                        'cci_%d_%s.tfrecords' % (index_records, clazz)))
+            convert_to_tfrecord(xml, record_writer)
+            num += 1
+        tf.io.gfile.rename(
+            'cci_%d_%s.tfrecords' % (index_records, clazz),
+            'cci_%d_%s_%d.tfrecords' % (index_records, clazz, num))
 
-            record_writer.close()
+        record_writer.close()
 
+            
